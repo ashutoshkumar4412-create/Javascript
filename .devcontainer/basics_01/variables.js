0553873fd3bb1baace62210783bb5075dba68885
ashutoshkumar4412-create <ashutoshkumar4412@gmail.com>
2026-03-10 08:51:23 +0000

    first done

---
+++ .devcontainer/basics_01/variables.js
@@ -0,0 +1,12 @@
+const accountId = 12334
+let accountEmail = "ashutoshkumargmail.com"
+accountCity = "bihar"
+let accountStates;
+
+
+accountEmail = "ashu@gamil.com"
+accountPassword = "ashu123"
+accountCity = "patna"
+
+
+console.table([accountId,accountPassword,accountCity])
