caef0a55269cae7b7eb9af0591ddde1e85a89ed3
Ashutosh kumar <ashutoshkumar4412@gmail.com>
2026-03-31 07:41:30 +0000

    object-02

---
+++ .devcontainer/basics_02/04_object.js
@@ -0,0 +1,58 @@
+// const tinderUser = new Object()
+const tinderUser = {}
+
+tinderUser.id = "123abs"
+tinderUser.name = "ashu"
+tinderUser.isLoggedIn = false
+
+//console.log(tinderUser)
+
+const regularUser = {
+    email: "who@gmail.com",
+    fullname: {
+        userfullname: {
+            firstname: "ashu",
+            lastname: "singh"
+        }
+    }
+}
+
+//console.log(regularUser.fullname.userfullname.firstname);
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+
+//const obj3 = { obj1,obj2}
+//const obj3 = Object.assign({},obj1, obj2)
+
+const obj3 = {...obj1,...obj2}
+//console.log(obj3)
+
+
+const users = [
+    {
+        id: 1,
+        email: "ashu@king.com"
+    },
+    {
+        id: 2,
+        email: "ashu@123.com"
+    },
+    {
+        id: 3,
+        email: "ashu@omg.com"
+    },
+    {
+    },
+    {
+    },
+]
+
+users[1].email
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser));   //important
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn'))
