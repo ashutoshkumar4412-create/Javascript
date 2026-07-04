0411f76a730f4231de9f6ee680acf0747c886eb7
Ashutosh kumar <ashutoshkumar4412@gmail.com>
2026-07-04 11:19:34 +0000

    switch_case-02

---
+++ .devcontainer/04_control_flow/switch.js
@@ -0,0 +1,25 @@
+//switch (key) {
+//  case 'a':
+  //  break;
+ // case 'b':
+ //  break;
+ //default:
+//   break;
+//}
+
+const month = 'c';
+
+switch (month) {
+  case 'a':
+     console.log('January');
+    break;
+  case 'b':
+     console.log('February');
+    break;
+  case 'c':
+     console.log('March');
+    break;
+  default:
+     console.log('Invalid month');
+    break;
+}
